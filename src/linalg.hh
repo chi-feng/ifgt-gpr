6975292efc4fb7e451ec08a4b05e84aac250685e
chi-feng <email.chi.feng@gmail.com>
2013-05-16 09:49:23 -0400

    Added comments/doc to linalg.hh

--- src/linalg.hh
+++ src/linalg.hh
@@ -7,9 +7,15 @@
 #include <math.h>
 #include <assert.h>
 
+/* poor-man's linear algebra */
 typedef std::vector<double> Vector;
 typedef std::vector<Vector> Matrix;
 
+/*! Matrix-vector multiplication 
+ * \param A The matrix
+ * \param x The vector
+ * \return The matrix-vector product between A and x
+ */
 inline Vector operator*(const Matrix& A, const Vector& x) {
     assert(A.size() > 0);
     assert(A[0].size() == x.size());
@@ -23,6 +29,11 @@ inline Vector operator*(const Matrix& A, const Vector& x) {
     return product;
 } 
 
+/*! Vector-matrix multiplication 
+ * \param x The vector
+ * \param A The Matirx
+ * \return The matrix-vector product between x and A
+ */
 inline Vector operator*(const Vector& x, const Matrix& A) {
     assert(A.size() > 0);
     assert(A.size() == x.size());
@@ -36,6 +47,10 @@ inline Vector operator*(const Vector& x, const Matrix& A) {
     return product;
 } 
 
+/*! Vector addition 
+ * \param a The first vector
+ * \param b The second vector
+ * \return The sum of the first and second vector */
 inline Vector operator+(const Vector& a, const Vector& b) {
     assert(a.size() > 0); 
     assert(b.size() > 0);
@@ -47,6 +62,11 @@ inline Vector operator+(const Vector& a, const Vector& b) {
     return sum;
 }
 
+
+/*! Vector subtraction 
+ * \param a The first vector
+ * \param b The second vector
+ * \return The difference of the first and second vector */
 inline Vector operator-(const Vector& a, const Vector& b) {
     assert(a.size() > 0); 
     assert(b.size() > 0);
@@ -58,6 +78,11 @@ inline Vector operator-(const Vector& a, const Vector& b) {
     return sum;
 }
 
+
+/*! Scalar multiplication of a vector 
+ * \param a The scalar
+ * \param x The vector
+ * \return The vector x scaled by scalar a */
 inline Vector operator*(const double a, const Vector& x) {
     assert(x.size() > 0); 
     assert(a == a); // for NaN 
@@ -68,6 +93,11 @@ inline Vector operator*(const double a, const Vector& x) {
     return product;
 }
 
+
+/*! Increment a vector by another vector
+ * \param a The vector
+ * \param b The vector to be added
+ * \return The reference to the original vector, incremented */
 inline Vector& operator+=(Vector& a, const Vector& b) {
     assert(a.size() > 0); 
     assert(b.size() > 0);
@@ -78,6 +108,10 @@ inline Vector& operator+=(Vector& a, const Vector& b) {
     return a;
 }
 
+/*! Decrement a vector by another vector
+ * \param a The vector
+ * \param b The vector to be subtracted
+ * \return The reference to the original vector, decremented */
 inline Vector& operator-=(Vector& a, const Vector& b) {
     assert(a.size() > 0); 
     assert(b.size() > 0);
@@ -88,6 +122,11 @@ inline Vector& operator-=(Vector& a, const Vector& b) {
     return a;
 }
 
+/*! Pointwise multiplication of two vectors
+ * \param a The first vector
+ * \param b The second vector
+ * \return The reference to the original vector, 
+ * multiplied pointwise by hte second vector */
 inline Vector& operator*=(Vector& a, const Vector& b) {
     assert(a.size() > 0); 
     assert(b.size() > 0);
@@ -98,6 +137,11 @@ inline Vector& operator*=(Vector& a, const Vector& b) {
     return a;
 }
 
+/*! Pointwise division of two vectors
+ * \param a The first vector
+ * \param b The second vector
+ * \return The reference to the original vector, 
+ * divided pointwise by hte second vector */
 inline Vector& operator/=(Vector& a, const Vector& b) {
     assert(a.size() > 0); 
     assert(b.size() > 0);
@@ -108,6 +152,11 @@ inline Vector& operator/=(Vector& a, const Vector& b) {
     return a;
 }
 
+
+/*! Inner (dot) product between two vectors
+ * \param a The first vector
+ * \param b The second vector
+ * \return The dot product between the first and second vector */
 inline double operator*(const Vector& a, const Vector& b) {
     assert(a.size() > 0); 
     assert(b.size() > 0);
@@ -119,6 +168,8 @@ inline double operator*(const Vector& a, const Vector& b) {
     return product;
 }
 
+/*! Prints a vector to the screen 
+ * \param x The vector to be printed */
 void printVector(const Vector& x) {
     std::cout.precision(3);
     std::cout << std::fixed;
@@ -131,6 +182,8 @@ void printVector(const Vector& x) {
     std::cout << std::endl;
 }
 
+/*! Prints a matrix to the screen 
+ * \param A The matrix to be printed */
 void printMatrix(const Matrix& A) {
     std::cout.precision(3);
     std::cout << std::fixed;
@@ -142,6 +195,8 @@ void printMatrix(const Matrix& A) {
     }
 }
 
+/*! Prints a matrix to the screen 
+ * \param A The matrix to be printed */
 double maxValue(const Vector& x) {
     double max = x[0];
     for (size_t i = 1; i < x.size(); i++) {
@@ -196,7 +251,13 @@ void invertMatrix(const Matrix& A, Matrix& Ainv) {
      
 }
 
-// Solve matrix equation using conjugate-gradient
+/*! Solve matrix equation Ax=b using conjugate-gradient method
+ * \param A The matrix
+ * \param b The rhs
+ * \param x The initial guess for x
+ * \param epsilon The threshold for epsilon_2 to exit inner loop
+ * \param MAX_ITER the maximum number of iterations
+ * \return The number of iterations */
 size_t invertMatrixCG(const Matrix& A, const Vector& b, Vector& x, const double epsilon = 1.0e-16, const size_t MAX_ITER = 1000) {
     Vector r(b.size()); // residue
     Vector p(b.size()); // std::vector along the search direction
